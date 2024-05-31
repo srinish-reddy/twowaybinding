8a17ead9b9fe732f68cc9a39cf2c7c3542bf1e1f
srinish-reddy <160460434+srinish-reddy@users.noreply.github.com>
2024-05-31 14:13:12 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '2waybinding';
+  showTitleChange(){
+    console.log(this.title);
+  }
+  
+}
